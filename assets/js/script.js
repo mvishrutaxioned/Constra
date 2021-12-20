e16d022f4daa8c9cc431fb88d1bb5dd5533bd9b9
VishrutMargam <vishrutm@axioned.com>
2021-12-20 11:24:55 +0530

    script declare variables

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+
+    // variable declarations
+    var content = '';
+    var regexEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    var topHeight = $('header').height();
+    var bannerHeight = $('.banner').height();
+    var abHeight = $('.about').height();
+    var callCounter = topHeight + bannerHeight/3 + abHeight;
+
+    var data = [
+        {
+            title: 'WE JUST COMPLETES $17.6 MILLION MEDICAL CLINIC IN MID-MISSOURI',
+            date: 'JULY 20, 2017',
+            image: '../assets/Images/news/news1.jpg'
+        },
+        {
+            title: 'THANDLER AIRPORT WATER RECLAMATION FACILITY EXPANSION PROJECT NAMED',
+            date: 'JUNE 17, 2017',
+            image: '../assets/Images/news/news2.jpg'
+        },
+        {
+            title: 'SILICON BENCH AND CORNIKE BEGIN CONSTRUCTION SOLAR FACILITIES',
+            date: 'AUG 13, 2017',
+            image: '../assets/Images/news/news3.jpg'
+        }
+    ]
+})
